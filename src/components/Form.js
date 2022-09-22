946001dde92678793a96b50f613034aa8bfca4f2
Edgard <20133252@ealoe.ulima.edu.pe>
2022-09-22 14:23:25 -0500

    adding empty branch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,69 +1,34 @@
 import React from "react"
 
 export default function Form() {
-    const [formData, setFormData] = React.useState(
-        {
-            firstName: "", 
-            lastName: "", 
-            email: "", 
-            comments: "", 
-            isFriendly: true,
-            employment: "",
-            favColor: ""
-        }
-    )
     
-    function handleChange(event) {
-        const {name, value, type, checked} = event.target
-        setFormData(prevFormData => {
-            return {
-                ...prevFormData,
-                [name]: type === "checkbox" ? checked : value
-            }
-        })
-    }
-    
-    function handleSubmit(event) {
-        event.preventDefault()
-        // submitToApi(formData)
-        console.log(formData)
-    }
     
     return (
-        <form onSubmit={handleSubmit}>
+        <form>
             <input
                 type="text"
                 placeholder="First Name"
-                onChange={handleChange}
-                name="firstName"
-                value={formData.firstName}
+             
             />
             <input
                 type="text"
                 placeholder="Last Name"
-                onChange={handleChange}
-                name="lastName"
-                value={formData.lastName}
+                
             />
             <input
                 type="email"
                 placeholder="Email"
-                onChange={handleChange}
-                name="email"
-                value={formData.email}
+               
             />
             <textarea 
-                value={formData.comments}
+               
                 placeholder="Comments"
-                onChange={handleChange}
-                name="comments"
+                
             />
             <input 
                 type="checkbox" 
                 id="isFriendly" 
-                checked={formData.isFriendly}
-                onChange={handleChange}
-                name="isFriendly"
+               
             />
             <label htmlFor="isFriendly">Are you friendly?</label>
             <br />
@@ -74,10 +39,7 @@ export default function Form() {
                 <input 
                     type="radio"
                     id="unemployed"
-                    name="employment"
-                    value="unemployed"
-                    checked={formData.employment === "unemployed"}
-                    onChange={handleChange}
+                    
                 />
                 <label htmlFor="unemployed">Unemployed</label>
                 <br />
@@ -85,10 +47,7 @@ export default function Form() {
                 <input 
                     type="radio"
                     id="part-time"
-                    name="employment"
-                    value="part-time"
-                    checked={formData.employment === "part-time"}
-                    onChange={handleChange}
+                    
                 />
                 <label htmlFor="part-time">Part-time</label>
                 <br />
@@ -96,10 +55,7 @@ export default function Form() {
                 <input 
                     type="radio"
                     id="full-time"
-                    name="employment"
-                    value="full-time"
-                    checked={formData.employment === "full-time"}
-                    onChange={handleChange}
+                   
                 />
                 <label htmlFor="full-time">Full-time</label>
                 <br />
@@ -110,9 +66,7 @@ export default function Form() {
             <br />
             <select 
                 id="favColor" 
-                value={formData.favColor}
-                onChange={handleChange}
-                name="favColor"
+                
             >
                 <option value="red">Red</option>
                 <option value="orange">Orange</option>
